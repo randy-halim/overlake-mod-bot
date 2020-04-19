fb679973d819cca9736e6f19485fcf93f73160b2
Randolph Halim <halimr@Mounts-MacBook-Pro.local>
2020-04-18 19:54:15 -0700

    eslint and initial setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+client.login("NjIxMTc3NTM5NzY2OTEwOTc2.XpuxXg.4eKcO6dQP9zu9spUOrrGA_YUSPA");
