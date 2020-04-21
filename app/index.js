56aa9d8331a546f81138da45da967da67cd69c44
daniel <daniel@daniel>
2020-04-20 23:55:30 -0700

    Added wrapper for discord client

--- app/index.js
+++ app/index.js
@@ -2,14 +2,11 @@ global.__rootdir = __dirname;
 // Read environment variables
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require('fs');
 const path = require('path');
-const logger = require(__rootdir + '/util/logger.js');
 
-// Begin periodic logging
-logger.begin();
+const logger = require(__rootdir + '/util/logger.js');
+const client = require(__rootdir + '/util/client.js');
 
 // Setup event handler synchronously
 const files = fs.readdirSync(__rootdir + '/events/');
@@ -18,4 +15,6 @@ for (const file of files) {
     client.on(event, require(path.join(__rootdir, 'events', file)));
 }
 
+// Begin periodic logging
+logger.begin();
 client.login(process.env.DISCORD_TOKEN);
