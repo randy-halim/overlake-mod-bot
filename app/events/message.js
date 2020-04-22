d84335d45fc2f43f3b98391abedeb3ff34468f53
daniel <daniel@daniel>
2020-04-21 20:13:33 -0700

    added command handler

--- app/events/message.js
+++ app/events/message.js
@@ -1,5 +1,18 @@
 const logger = require(__rootdir + '/util/logger.js');
+const { client, commands } = require(__rootdir + '/util/client-wrapper.js');
+
+// This will eventually go into the config file(s)
+const prefix = '!';
 module.exports = (message) => {
     logger.log(`${message.member.displayName} in channel ${message.channel} at ${Date()}:\n${message.content}\n\n`);
+
+    if (!message.content.startsWith(prefix)) {
+        return;
+    }
+    const command = message.content.substring(1);
+    if (!commands.has(command)) {
+        return;
+    }
+    commands.get(command)(message);
 };
 
---
+++ app/util/client-wrapper.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const commands = new Map();
+module.exports = {
+    client,
+    commands
+};
