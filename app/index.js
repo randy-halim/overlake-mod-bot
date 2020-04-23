4ebd28aa8055c279889e5c2c22ec302d7c29e92a
Brownie-in-Motion <flintpeanutbutter@gmail.com>
2020-04-22 18:32:17 -0700

    Revert "fix/node modules"

--- app/index.js
+++ app/index.js
@@ -1,24 +1,16 @@
 global.__rootdir = __dirname;
-
-// Read environment variables for development
+// Read environment variables
 require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
 
 const logger = require(__rootdir + '/util/logger.js');
-const { client, commands } = require(__rootdir + '/util/client-wrapper.js');
-
-// Dynamically load commands
-const commandFiles = fs.readdirSync(__rootdir + '/commands/');
-for (const file of commandFiles) {
-    const command = require(path.join(__rootdir, 'commands', file));
-    commands.set(command.name, command);
-}
+const client = require(__rootdir + '/util/client.js');
 
 // Setup event handler synchronously by parsing each .js file in ../app/events/
-const eventFiles = fs.readdirSync(__rootdir + '/events/');
-for (const file of eventFiles) {
+const files = fs.readdirSync(__rootdir + '/events/');
+for (const file of files) {
     const event = path.parse(file).name;
     client.on(event, require(path.join(__rootdir, 'events', file)));
 }
